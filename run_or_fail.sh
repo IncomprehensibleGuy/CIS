5ee66bf54c8a9d3bb16c16adc835ed12426c24aa
IncomprehensibleGuy <gregoryrulit@gmail.com>
2020-03-14 13:06:10 +0300

    Add all files commit

--- run_or_fail.sh
+++ run_or_fail.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+# helper method for providing error messages for a command
+run_or_fail() {
+  local explanation=$1
+  shift 1
+  "$@"
+  if [ $? != 0 ]; then
+    echo $explanation 1>&2
+    exit 1
+  fi
+}
