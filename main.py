f369fe1b0ed6f31237bf14ba61389f0e023a711a
IncomprehensibleGuy <gregoryrulit@gmail.com>
2020-04-29 10:10:24 +0300

    main.py ready

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import os
+if __name__ == "__main__":
+    import os
 
 
-def main():
-    repo_clone_runner_path = 'C:/Users/Greg/Desktop/Projects/CIS/monitoring_repo/repo_clone_runner'
-    repo_clone_obs_path = 'C:/Users/Greg/Desktop/Projects/CIS/monitoring_repo/repo_clone_obs'
+    # settings
+    repo_path = 'C:/Users/Greg/Desktop/Projects/CIS/monitoring_repo/'
+    repo_clone_runner_path = repo_path + 'repo_clone_runner'
+    repo_clone_obs_path = repo_path + 'repo_clone_obs'
+
+    n_test_runners = 1
 
     start_dispatcher_command = 'dispatcher.py'
     start_test_runner_command = 'test_runner.py ' + repo_clone_runner_path
     start_repo_observer_command = 'repo_observer.py ' + repo_clone_obs_path
 
+
+    # run system
     os.system('start cmd /K python ' + start_dispatcher_command)
     os.system('start cmd /K python ' + start_repo_observer_command)
-    os.system('start cmd /K python ' + start_test_runner_command)
-
-
-if __name__ == "__main__":
-    main()
+    for n in range(n_test_runners):
+        os.system('start cmd /K python ' + start_test_runner_command)
