246cdab6dead245fd9f81893ef98a5ac8117d103
IncomprehensibleGuy <gregoryrulit@gmail.com>
2020-04-29 13:13:53 +0300

    test_runner.py ready 1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     repo_path = 'C:/Users/Greg/Desktop/Projects/CIS/monitoring_repo/'
     repo_clone_runner_path = repo_path + 'repo_clone_runner'
 
-    n_test_runners = 5
+    n_test_runners = 1
 
     start_dispatcher_command = 'dispatcher.py'
     start_test_runner_command = 'test_runner.py ' + repo_clone_runner_path
--- repo_observer.py
+++ repo_observer.py
@@ -6,51 +6,41 @@ import time
 import helpers
 
 
-def poll():
+def push_commit_to_tests():
     # Settings
     dispatcher_host = 'localhost'
     dispatcher_port = 8888
-
-    # Paste path to the repo on your machine
     observing_repo_clone = 'C:/Users/Greg/Desktop/Projects/CIS/monitoring_repo/repo_clone_obs'
 
-    while True:
+    # Call the bash script that will update the repo and check for changes.
+    # If there's a change, it will drop a .commit_id file with the latest commit in the current working dir.
+    try:
+        subprocess.check_output(['update_repo.sh', observing_repo_clone], shell=True)
+    except subprocess.CalledProcessError as e:
+        raise Exception(f'Could not update and check repository. Reason: {e.output}')
+
+    if os.path.isfile('.commit_id'):
+        # We have a change -> execute the tests
+        # First, check the status of the dispatcher server to see if we can send the tests
         try:
-            # call the bash script that will update the repo and check
-            # for changes. If there's a change, it will drop a .commit_id file
-            # with the latest commit in the current working directory
-            subprocess.check_output(["update_repo.sh", observing_repo_clone], shell=True)
-        except subprocess.CalledProcessError as e:
-            raise Exception("Could not update and check repository. Reason: %s" % e.output)
+            response = helpers.communicate(dispatcher_host, dispatcher_port, b'status')
+            response =  response.decode('utf-8')
+        except socket.error as e:
+            raise Exception(f'Could not communicate with dispatcher server: {e}')
 
-        if os.path.isfile(".commit_id"):
-            # great, we have a change! let's execute the tests
-            # First, check the status of the dispatcher server to see
-            # if we can send the tests
-            try:
-                response = helpers.communicate(dispatcher_host, int(dispatcher_port), b"status")
-                response =  response.decode('utf-8')
-            except socket.error as e:
-                raise Exception("Could not communicate with dispatcher server: %s" % e)
-
-            if response == "OK":
-                # Dispatcher is present, let's   send it a test
-                commit = ""
-                with open(".commit_id", "r") as f:
-                    commit = f.readline()
-                response = helpers.communicate(dispatcher_host, int(dispatcher_port), ("dispatch:" + commit).encode())
-                response = response.decode('utf-8')
-
-                if response != "OK":
-                    raise Exception("Could not dispatch the test: %s" %
-                    response)
-                print("dispatched!")
-            else:
-                # Something wrong happened to the dispatcher
-                raise Exception("Could not dispatch the test: %s" %
-                response)
-        time.sleep(12)
+        if response == 'OK':
+            # Dispatcher is present -> send it a test
+            commit = ''
+            with open('.commit_id', 'r') as f:
+                commit = f.readline()
+            response = helpers.communicate(dispatcher_host, dispatcher_port, ('dispatch:' + commit).encode())
+            response = response.decode('utf-8')
+            if response != 'OK': raise Exception(f'Could not dispatch commit: {response}')
+            print("commit dispatched")
+        else:
+            # Something wrong happened to the dispatcher
+            raise Exception(f"Could not dispatch the test: {response}")
 
 
 if __name__ == "__main__":
-    poll()
+    push_commit_to_tests()
